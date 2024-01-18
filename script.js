dff70967b6513fa995dc714ad8e78ac44551e24f
Yasmeen <yomalbalooshi@gmail.com>
2024-01-18 14:59:17 +0300

    added player location and movement functions to board

--- script.js
+++ script.js
@@ -35,40 +35,66 @@ const game = {
   mode: 'daydream'
 }
 const player = {
+  //boardLocation syntax: [up/down, left/right]
   boardLocation: [0, 0],
   score: 0
 }
 
 const movePlayerUp = () => {
-  if (player.boardLocation[0] !== 0) {
+  if (
+    player.boardLocation[0] !== 0 &&
+    basicArray[player.boardLocation[0] - 1] !== 1
+  ) {
+    basicArray[player.boardLocation[0]][player.boardLocation[1]] = 0
     player.boardLocation[0] = player.boardLocation[0] - 1
+    basicArray[player.boardLocation[0]][player.boardLocation[1]] = 2
   }
 }
 const movePlayerDown = () => {
-  if (player.boardLocation[0] !== basicArray.length - 1) {
+  if (
+    player.boardLocation[0] !== basicArray.length - 1 &&
+    basicArray[player.boardLocation[0] + 1] !== 1
+  ) {
+    basicArray[player.boardLocation[0]][player.boardLocation[1]] = 0
     player.boardLocation[0] = player.boardLocation[0] + 1
+    basicArray[player.boardLocation[0]][player.boardLocation[1]] = 2
   }
 }
 const movePlayerRight = () => {
-  if (player.boardLocation[1] !== basicArray[0].length) {
+  if (
+    player.boardLocation[1] !== basicArray[0].length &&
+    basicArray[player.boardLocation[0]][player.boardLocation[1] + 1] !== 1
+  ) {
+    basicArray[player.boardLocation[0]][player.boardLocation[1]] = 0
     player.boardLocation[1] = player.boardLocation[1] + 1
+    basicArray[player.boardLocation[0]][player.boardLocation[1]] = 2
   }
 }
 const movePlayerLeft = () => {
-  if (player.boardLocation[1] !== 0) {
+  if (
+    player.boardLocation[1] !== 0 &&
+    basicArray[player.boardLocation[0]][player.boardLocation[1] - 1] !== 1
+  ) {
+    basicArray[player.boardLocation[0]][player.boardLocation[1]] = 0
     player.boardLocation[1] = player.boardLocation[1] - 1
+    basicArray[player.boardLocation[0]][player.boardLocation[1]] = 2
   }
 }
-// console.log(player.boardLocation)
-// movePlayerUp()
-// console.log(player.boardLocation)
-// movePlayerDown()
-// console.log(player.boardLocation)
-// movePlayerRight()
-// console.log(player.boardLocation)
+console.log(player.boardLocation)
+movePlayerUp()
+movePlayerRight()
+movePlayerRight()
+movePlayerDown()
+
+movePlayerRight()
+movePlayerDown()
+movePlayerRight()
+console.log(player.boardLocation)
+console.log(basicArray)
+
 // movePlayerLeft()
 // console.log(player.boardLocation)
-// movePlayerDown()
+
 // console.log(player.boardLocation)
 // movePlayerRight()
 // console.log(player.boardLocation)
