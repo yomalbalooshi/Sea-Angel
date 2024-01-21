fb074cc3302dc577b7183b5cd0c29b13bfb7613f
Yasmeen <yomalbalooshi@gmail.com>
2024-01-21 15:39:23 +0300

    added nightmare display

--- script.js
+++ script.js
@@ -14,9 +14,11 @@ let gameLevel = document.querySelector('#currentlevel')
 let hearts = document.querySelector('#currentLivesLeft')
 let playerScore = document.querySelector('#currentPoints')
 let playerKeys = document.querySelector('#currentKeys')
+
 import { Game } from './dataStore.js'
+
 let game = new Game('daydream')
-console.log(game)
+
 const player = {
   //boardLocation syntax: [up/down, left/right]
   boardLocation: [],
@@ -151,28 +153,55 @@ const displayNextLevelMenu = (currentLevel) => {
   document.querySelector('#game-container-div').innerHTML = ''
   let nextLevelMenu = document.createElement('div')
   nextLevelMenu.setAttribute('id', 'game-next-level-div')
+
   let nextLevelHeader = document.createElement('h3')
   nextLevelHeader.setAttribute('id', 'next-level-header')
   nextLevelHeader.innerText = 'Level Complete!'
+
+  let nextLevelStatsKeys = document.createElement('h4')
+  nextLevelStatsKeys.setAttribute('id', 'next-level-stats-keys')
+  nextLevelStatsKeys.innerText = `Keys Collected: ${
+    player.keys
+  }/${game.getLevelTotalKeys()}`
+
+  let nextLevelStatsScore = document.createElement('h4')
+  nextLevelStatsScore.setAttribute('id', 'next-level-stats-score')
+  nextLevelStatsScore.innerText = `Points Collected: ${
+    player.score
+  }/${game.getLevelTotalScore()}`
+
+  let nextLevelStatsLivesLeft = document.createElement('h4')
+  nextLevelStatsLivesLeft.setAttribute('id', 'next-level-stats-lives-left')
+  nextLevelStatsLivesLeft.innerText = `Lives left: ${player.lives}`
+
   let restartLevelButton = document.createElement('button')
   restartLevelButton.setAttribute('id', 'restart')
   restartLevelButton.innerText = 'Restart Level'
+
   let nextLevelButton = document.createElement('button')
   nextLevelButton.setAttribute('id', 'next-level-button')
   nextLevelButton.innerText = 'Next Level'
+
   nextLevelMenu.appendChild(nextLevelHeader)
+  nextLevelMenu.appendChild(nextLevelStatsLivesLeft)
+  nextLevelMenu.appendChild(nextLevelStatsKeys)
+  nextLevelMenu.appendChild(nextLevelStatsScore)
+
   nextLevelMenu.appendChild(restartLevelButton)
   nextLevelMenu.appendChild(nextLevelButton)
   document.querySelector('#game-container-div').appendChild(nextLevelMenu)
+
   restartLevelButton.addEventListener('click', () => {
     startLevel(game.gameBoards[game.level - 1].board)
   })
   nextLevelButton.addEventListener('click', () => {
     game.updateLevel()
     game.updateAccumTotals(player.score, player.keys)
+
     startLevel(game.gameBoards[game.level - 1].board)
   })
 }
+
 const displayEndGameMenu = () => {
   document.querySelector('#game-container-div').innerHTML = ''
   let endGameMenu = document.createElement('div')
@@ -180,6 +209,19 @@ const displayEndGameMenu = () => {
   let endGameMenuHeader = document.createElement('h3')
   endGameMenuHeader.setAttribute('id', 'end-game-header')
   endGameMenuHeader.innerText = 'You escaped!'
+
+  let endGameStatsKeys = document.createElement('h4')
+  endGameStatsKeys.setAttribute('id', 'end-game-stats-keys')
+  endGameStatsKeys.innerText = `Keys Collected: ${game.accumKeys}/${game.totalGameKeys}`
+
+  let endGameStatsScore = document.createElement('h4')
+  endGameStatsScore.setAttribute('id', 'end-game-stats-score')
+  endGameStatsScore.innerText = `Points Collected: ${game.accumScore}/${game.totalGameScore}`
+
+  let endGameStatsDeaths = document.createElement('h4')
+  endGameStatsDeaths.setAttribute('id', 'end-game-stats-deaths')
+  endGameStatsDeaths.innerText = `Deaths: ${game.accumDeaths}`
+
   let restartLevelButton = document.createElement('button')
   restartLevelButton.setAttribute('id', 'restart')
   restartLevelButton.innerText = 'Restart Level'
@@ -187,18 +229,23 @@ const displayEndGameMenu = () => {
     startLevel(game.gameBoards[game.level - 1].board)
   })
   endGameMenu.appendChild(endGameMenuHeader)
+  endGameMenu.appendChild(endGameStatsKeys)
+  endGameMenu.appendChild(endGameStatsScore)
+  endGameMenu.appendChild(endGameStatsDeaths)
   endGameMenu.appendChild(restartLevelButton)
   document.querySelector('#game-container-div').appendChild(endGameMenu)
 }
 
 const endGame = (gameStatus) => {
   if (gameStatus === 'death') {
-    startLevel(game.boardArray)
+    game.updateDeaths()
+    startLevel(structuredClone(game.gameBoards[game.level - 1]).board)
   } else if (gameStatus === 'exit') {
-    if (player.keys < game.totalKeys) {
+    if (player.keys < game.levelExitKeys) {
       alert('missing some stuff')
     } else {
       if (game.level === game.gameBoards.length) {
+        game.updateAccumTotals(player.score, player.keys)
         displayEndGameMenu()
       } else {
         displayNextLevelMenu(game.level)
@@ -236,9 +283,9 @@ const checkIfKey = () => {
 }
 
 const checkLocation = () => {
-  checkIfTrap()
   checkIfScore()
   checkIfKey()
+  checkIfTrap()
 }
 document.querySelector('#game-container-div').innerHTML = ''
 startLevel(game.gameBoards[0].board)
--- scriptNightmare.js
+++ scriptNightmare.js
@@ -6,6 +6,7 @@
 // 4 signifies trap
 // 5 signifies points
 // 6 signifies keys
+
 let gameMode = document.querySelector('#daydreamMode')
 gameMode.addEventListener('click', () => {
   window.location.href = './index.html'
@@ -14,17 +15,11 @@ let gameLevel = document.querySelector('#currentlevel')
 let hearts = document.querySelector('#currentLivesLeft')
 let playerScore = document.querySelector('#currentPoints')
 let playerKeys = document.querySelector('#currentKeys')
-import { daydream } from './dataStore.js'
-const game = {
-  level: 1,
-  mode: 'daydream',
-  boardArray: structuredClone(daydream[0]),
-  gameBoards: structuredClone(daydream),
-  playerStartPosition: [1, 1],
-  trapPower: 1,
-  totalScore: 5,
-  totalKeys: 2
-}
+
+import { Game } from './dataStore.js'
+
+let game = new Game('nightmare')
+
 const player = {
   //boardLocation syntax: [up/down, left/right]
   boardLocation: [],
@@ -40,6 +35,7 @@ const movePlayerUp = () => {
   ) {
     game.boardArray[player.boardLocation[0]][player.boardLocation[1]] = 0
     player.boardLocation[0] = player.boardLocation[0] - 1
+
     checkLocation()
     if (checkIfExit() == true) {
       endGame('exit')
@@ -118,21 +114,33 @@ const displayBoard = () => {
     for (let j = 0; j < game.boardArray[i].length; j++) {
       const boardElement = document.createElement('div')
       boardElement.classList.add('board-element')
-      if (game.boardArray[i][j] === 2) {
-        boardElement.classList.add('board-player')
-      } else if (game.boardArray[i][j] === 0) {
-        boardElement.classList.add('board-floor-light')
-      } else if (game.boardArray[i][j] === 1) {
-        boardElement.classList.add('board-wall-light')
-      } else if (game.boardArray[i][j] === 3) {
-        boardElement.classList.add('board-exit-light')
-      } else if (game.boardArray[i][j] === 4) {
-        boardElement.classList.add('board-trap-light')
-      } else if (game.boardArray[i][j] === 5) {
-        boardElement.classList.add('board-score-light')
-      } else if (game.boardArray[i][j] === 6) {
-        boardElement.classList.add('board-key-light')
+      if (
+        (i === player.boardLocation[0] ||
+          i - 1 === player.boardLocation[0] ||
+          i + 1 === player.boardLocation[0]) &&
+        (j === player.boardLocation[1] ||
+          j - 1 === player.boardLocation[1] ||
+          j + 1 === player.boardLocation[1])
+      ) {
+        if (game.boardArray[i][j] === 2) {
+          boardElement.classList.add('board-player')
+        } else if (game.boardArray[i][j] === 0) {
+          boardElement.classList.add('board-floor-light')
+        } else if (game.boardArray[i][j] === 1) {
+          boardElement.classList.add('board-wall-light')
+        } else if (game.boardArray[i][j] === 3) {
+          boardElement.classList.add('board-exit-light')
+        } else if (game.boardArray[i][j] === 4) {
+          boardElement.classList.add('board-trap-light')
+        } else if (game.boardArray[i][j] === 5) {
+          boardElement.classList.add('board-score-light')
+        } else if (game.boardArray[i][j] === 6) {
+          boardElement.classList.add('board-key-light')
+        }
+      } else {
+        boardElement.classList.add('board-other-dark')
       }
+
       gameBoard.appendChild(boardElement)
     }
   }
@@ -159,27 +167,55 @@ const displayNextLevelMenu = (currentLevel) => {
   document.querySelector('#game-container-div').innerHTML = ''
   let nextLevelMenu = document.createElement('div')
   nextLevelMenu.setAttribute('id', 'game-next-level-div')
+
   let nextLevelHeader = document.createElement('h3')
   nextLevelHeader.setAttribute('id', 'next-level-header')
   nextLevelHeader.innerText = 'Level Complete!'
+
+  let nextLevelStatsKeys = document.createElement('h4')
+  nextLevelStatsKeys.setAttribute('id', 'next-level-stats-keys')
+  nextLevelStatsKeys.innerText = `Keys Collected: ${
+    player.keys
+  }/${game.getLevelTotalKeys()}`
+
+  let nextLevelStatsScore = document.createElement('h4')
+  nextLevelStatsScore.setAttribute('id', 'next-level-stats-score')
+  nextLevelStatsScore.innerText = `Points Collected: ${
+    player.score
+  }/${game.getLevelTotalScore()}`
+
+  let nextLevelStatsLivesLeft = document.createElement('h4')
+  nextLevelStatsLivesLeft.setAttribute('id', 'next-level-stats-lives-left')
+  nextLevelStatsLivesLeft.innerText = `Lives left: ${player.lives}`
+
   let restartLevelButton = document.createElement('button')
   restartLevelButton.setAttribute('id', 'restart')
   restartLevelButton.innerText = 'Restart Level'
+
   let nextLevelButton = document.createElement('button')
   nextLevelButton.setAttribute('id', 'next-level-button')
   nextLevelButton.innerText = 'Next Level'
+
   nextLevelMenu.appendChild(nextLevelHeader)
+  nextLevelMenu.appendChild(nextLevelStatsLivesLeft)
+  nextLevelMenu.appendChild(nextLevelStatsKeys)
+  nextLevelMenu.appendChild(nextLevelStatsScore)
+
   nextLevelMenu.appendChild(restartLevelButton)
   nextLevelMenu.appendChild(nextLevelButton)
   document.querySelector('#game-container-div').appendChild(nextLevelMenu)
+
   restartLevelButton.addEventListener('click', () => {
-    startLevel(game.gameBoards[game.level - 1])
+    startLevel(game.gameBoards[game.level - 1].board)
   })
   nextLevelButton.addEventListener('click', () => {
-    game.level++
-    startLevel(game.gameBoards[game.level - 1])
+    game.updateLevel()
+    game.updateAccumTotals(player.score, player.keys)
+
+    startLevel(game.gameBoards[game.level - 1].board)
   })
 }
+
 const displayEndGameMenu = () => {
   document.querySelector('#game-container-div').innerHTML = ''
   let endGameMenu = document.createElement('div')
@@ -187,25 +223,43 @@ const displayEndGameMenu = () => {
   let endGameMenuHeader = document.createElement('h3')
   endGameMenuHeader.setAttribute('id', 'end-game-header')
   endGameMenuHeader.innerText = 'You escaped!'
+
+  let endGameStatsKeys = document.createElement('h4')
+  endGameStatsKeys.setAttribute('id', 'end-game-stats-keys')
+  endGameStatsKeys.innerText = `Keys Collected: ${game.accumKeys}/${game.totalGameKeys}`
+
+  let endGameStatsScore = document.createElement('h4')
+  endGameStatsScore.setAttribute('id', 'end-game-stats-score')
+  endGameStatsScore.innerText = `Points Collected: ${game.accumScore}/${game.totalGameScore}`
+
+  let endGameStatsDeaths = document.createElement('h4')
+  endGameStatsDeaths.setAttribute('id', 'end-game-stats-deaths')
+  endGameStatsDeaths.innerText = `Deaths: ${game.accumDeaths}`
+
   let restartLevelButton = document.createElement('button')
   restartLevelButton.setAttribute('id', 'restart')
   restartLevelButton.innerText = 'Restart Level'
   restartLevelButton.addEventListener('click', () => {
-    startLevel(game.gameBoards[game.level - 1])
+    startLevel(game.gameBoards[game.level - 1].board)
   })
   endGameMenu.appendChild(endGameMenuHeader)
+  endGameMenu.appendChild(endGameStatsKeys)
+  endGameMenu.appendChild(endGameStatsScore)
+  endGameMenu.appendChild(endGameStatsDeaths)
   endGameMenu.appendChild(restartLevelButton)
   document.querySelector('#game-container-div').appendChild(endGameMenu)
 }
 
 const endGame = (gameStatus) => {
   if (gameStatus === 'death') {
-    startLevel(game.boardArray)
+    game.updateDeaths()
+    startLevel(structuredClone(game.gameBoards[game.level - 1]).board)
   } else if (gameStatus === 'exit') {
-    if (player.keys < game.totalKeys) {
+    if (player.keys < game.levelExitKeys) {
       alert('missing some stuff')
     } else {
       if (game.level === game.gameBoards.length) {
+        game.updateAccumTotals(player.score, player.keys)
         displayEndGameMenu()
       } else {
         displayNextLevelMenu(game.level)
@@ -243,10 +297,9 @@ const checkIfKey = () => {
 }
 
 const checkLocation = () => {
-  checkIfTrap()
   checkIfScore()
   checkIfKey()
+  checkIfTrap()
 }
-
 document.querySelector('#game-container-div').innerHTML = ''
-startLevel(game.gameBoards[0])
+startLevel(game.gameBoards[0].board)
